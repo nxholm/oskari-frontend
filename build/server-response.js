4c7f27f050b886ee36a40093b52954521338a01f
nyholmi <ilkka.nyholm@maanmittauslaitos.fi>
2017-09-22 16:07:00 +0300

    self-invoking

--- build/server-response.js
+++ build/server-response.js
@@ -1,19 +1,16 @@
 import axios from 'axios';
 import fs from 'fs';
 
-function response() {
+( function() {
     console.log("getting data");
     axios.get('http://localhost:7070/action?action_route=GetAppSetup', {
 
     })
     .then(function (response) {
-        console.log(response.data.startupSequence);
       var start = response.data.startupSequence;
       fs.writeFile('startupSequence.json', JSON.stringify(start));
     })
     .catch(function (error) {
       console.log(error);
     });
-  }
-
-response();
+} )();
